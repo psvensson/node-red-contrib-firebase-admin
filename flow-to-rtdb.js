0af1a1b15a6cf16ce70d9e62fe6f731cac1db8e7
Peter Svensson <psvensson@gmail.com>
2019-06-23 20:11:05 +0200

    added flow rtdb saving nodes

---
+++ flow-to-rtdb.js
@@ -0,0 +1,41 @@
+const runtime = require('node-red').runtime
+
+module.exports = function(RED) {
+  function FirebaseAdmin(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+
+    if(config.cred){
+      let c = RED.nodes.getNode(config.cred)
+      this.admin = c.admin
+    }
+
+    node.on('input', function(msg) {
+      if(msg && msg.payload){
+        console.log('flow-to-rtdb got input')
+        console.dir(msg)
+        this.path = msg.payload.path || config.path
+        this.flowId = msg.payload.flowId || config.flowId
+        console.log('flow-to-rtdb path='+this.path+', flowId='+this.flowId)
+
+        runtime.flows.getFlow({id: this.flowId}).then((flow)=>{
+          this.admin.database().ref(this.path).set(flow).then((res)=>{
+            console.log('firebase set result '+res)
+            console.dir(res)
+            node.send({payload: res})
+          },(err)=>{
+            console.log('rtdb saving error')
+            console.dir(err)
+          }).catch((ex)=>{
+            console.log('rtdb saving exception')
+            console.dir(ex)
+          })
+        })
+
+      }
+    }.bind(this));
+
+
+  }
+  RED.nodes.registerType("flow-to-rtdb", FirebaseAdmin);
+}
---
+++ rtdb-to-flow.js
@@ -0,0 +1,53 @@
+const runtime = require('node-red').runtime
+
+let oldpath
+
+module.exports = function(RED) {
+  function FirebaseAdmin(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+
+    const cb = (res)=>{
+      //console.log('firebase get result '+res)
+      //console.dir(res)
+      let val = res.val()
+      runtime.flows.updateFlow({id: val.id, flow: val})
+      //console.log('val='+val)
+      node.send({payload:val})
+    }
+
+    let setUpListener = (path)=>{
+      //console.log('rtdb-get setUpListener for path '+path)
+      if(oldpath){
+        this.admin.database().ref(oldpath).off('value', cb)
+      }
+      this.admin.database().ref(path).on('value', cb)
+      oldpath = path
+    }
+
+    if(config.cred){
+      let c = RED.nodes.getNode(config.cred)
+      this.admin = c.admin
+    }
+
+    //console.log('------------------------------- rtdg-get config')
+    //console.dir(config)
+    this.path = config.path
+    if(this.path){
+      setUpListener(this.path)
+    }
+
+
+    //console.log('configuring rtdb-get to listen for messages')
+    node.on('input', function(msg) {
+      let path = this.path
+      if(msg && msg.payload){
+        path = msg.payload.path
+        setUpListener(path)
+      }
+    }.bind(this));
+
+
+  }
+  RED.nodes.registerType("rtdb-to-flow", FirebaseAdmin);
+}
