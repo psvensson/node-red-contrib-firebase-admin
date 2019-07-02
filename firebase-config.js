792374e1c88677519cc181fbed303e0449a743e7
Peter Svensson <psvensson@gmail.com>
2019-07-02 07:05:40 +0200

    first version of a cloud storage read node

--- firebase-config.js
+++ firebase-config.js
@@ -1,25 +1,42 @@
 const _admin = require('firebase-admin')
+const {Storage} = require('@google-cloud/storage');
+
+
 let init = false
+let s
 
 module.exports = function(RED) {
   function FirebaseConfigNode(n) {
     RED.nodes.createNode(this,n);
+    //------------------------------
     this.cred = n.cred
     this.dburl = n.dburl
-    this.sturl = n.sturl
+    this.bucket = n.bucket
+    //------------------------------
     this.admin = _admin
+    this.storage = s
     if(!init){
       console.log('setting admin....')
-      init = true
       this.credentials = JSON.parse(this.cred);
-      this.dburl = this.dburl
-      this.sturl = this.sturl
+      let credobj = _admin.credential.cert(this.credentials)
+      //console.dir(credobj)
+      //process.env.GOOGLE_APPLICATION_CREDENTIALS = 'creds.json'
+      init = true
+
       console.log('*** parsed firebase credentials: '+this.credentials.type+', project-id: '+this.credentials.project_id)
       _admin.initializeApp({
-        credential: _admin.credential.cert(this.credentials),
-        databaseURL: this.dburl,
-        storageBucket: this.sturl
+        credential: credobj,
+        databaseURL: this.dburl
       });
+      console.log('setting storage....')
+
+      s = new Storage({
+        //projectId: 'something-2e584',
+        //email: 'firebase-adminsdk-d1xiy@something-2e584.iam.gserviceaccount.com',
+        credentials: this.credentials
+      })
+
+      //s = new Storage()
     }
   }
   RED.nodes.registerType("firebase-config", FirebaseConfigNode);
---
+++ storage-read.js
@@ -0,0 +1,58 @@
+
+
+let oldpath
+
+module.exports = function(RED) {
+
+  function FirebaseAdmin(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+
+
+    if(config.cred){
+      let c = RED.nodes.getNode(config.cred)
+      this.admin = c.admin
+      this.storage = c.storage
+      this.bucket = config.bucket || c.bucket
+      this.path = config.path
+    }
+
+    console.log('------------------------------- rtdg-get config')
+    console.dir(config)
+
+    //console.log('configuring storage-read to listen for messages')
+    node.on('input', function(msg) {
+      if(msg && msg.payload){
+        let path = msg.payload.path || this.path
+        let bucket = msg.payload.bucket || this.bucket
+        console.log('storage-read reading from bucket "'+bucket+'" path "'+path+'"')
+
+        this.storage.getBuckets().then((results)=>{
+          const [buckets] = results;
+
+          console.log('Buckets:');
+          buckets.forEach(bucket => {
+            console.log(bucket.name);
+          });
+
+
+          this.storage
+          .bucket(bucket)
+          .file(path).download().then((file)=>{
+            console.log('storage-read got file')
+            //console.dir(file)
+            node.send({payload:file})
+
+          })
+
+
+        })
+
+
+      }
+    }.bind(this));
+
+
+  }
+  RED.nodes.registerType("storage-read", FirebaseAdmin);
+}
