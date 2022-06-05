e13a7aa7deae017388552adf452b8570272d957f
Peter Svensson <psvensson@gmail.com>
2022-06-05 11:06:38 +0200

    fixed path and unsub problem in firestore-on

--- firestore/firestore-on.js
+++ firestore/firestore-on.js
@@ -1,51 +1,50 @@
+let msgin;
+let unsub;
 
-let msgin
-let unsub
-
-module.exports = function(RED) {
+module.exports = function (RED) {
   function FirebaseAdmin(config) {
     RED.nodes.createNode(this, config);
     var node = this;
 
-    if(config.cred){
-      let c = RED.nodes.getNode(config.cred)
-      this.admin = c.admin
+    if (config.cred) {
+      let c = RED.nodes.getNode(config.cred);
+      this.admin = c.admin;
     }
 
-    const setup = ()=>{
-      if(unsub){
-        unsub()
+    const setup = (path) => {
+      if (unsub) {
+        unsub();
       }
-      this.admin.firestore().doc(path).onSnapshot(cb)
-    }
+      unsub = this.admin.firestore().doc(path).onSnapshot(cb);
+    };
 
-    const cb = (res)=>{
-      console.log('firestore get result '+res)
-      console.dir(res)
-      let val = res.data()
-      console.log('val='+val)
-      if(msgin){
-        msgin.payload = val
-        node.send(msgin)
+    const cb = (res) => {
+      console.log("firestore get result " + res);
+      console.dir(res);
+      let val = res.data();
+      console.log("val=" + val);
+      if (msgin) {
+        msgin.payload = val;
+        node.send(msgin);
       } else {
-        node.send({payload: val})
+        node.send({ payload: val });
       }
+    };
+
+    node.on(
+      "input",
+      function (msg) {
+        if (msg && msg.payload) {
+          msgin = msg;
+          const path = msg.payload.path;
+          setup(path);
+        }
+      }.bind(this)
+    );
+
+    if (config.path) {
+      setup(config.path);
     }
-
-    node.on('input', function(msg) {
-      if(msg && msg.payload){
-        msgin = msg
-        const path = msg.payload.path
-        setup(path)
-
-      }
-    }.bind(this));
-
-    if(config.path){
-      setup(config.path)
-    }
-
-
   }
   RED.nodes.registerType("firestore-on", FirebaseAdmin);
-}
+};
