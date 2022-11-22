65e58b4f7f4647fd14cfa610303c8d50b9a96a7a
Peter Svensson <psvensson@gmail.com>
2022-11-22 14:31:20 +0100

    resetting msgin after each send, so not to repeat last successful value when no new incoing values from rtdb

--- rtdb/rtdb-get.js
+++ rtdb/rtdb-get.js
@@ -1,64 +1,65 @@
+let msgin;
 
-let msgin
-
-module.exports = function(RED) {
-
+module.exports = function (RED) {
   function FirebaseAdmin(config) {
     RED.nodes.createNode(this, config);
     var node = this;
 
-    const cb = (res)=>{
-      console.log('firebase rtdb-get result '+res)
-      //console.dir(res)
-      let val = res.val()
-      //console.dir(val)
-      if(msgin){
-        msgin.payload = val
-        node.send(msgin)
+    const cb = (res) => {
+      console.log("firebase rtdb-get result " + JSON.stringify(res));
+      let val = res.val();
+      if (msgin) {
+        msgin.payload = val;
+        node.send(msgin);
       } else {
-        node.send({payload: val})
+        node.send({ payload: val });
       }
-    }
+      msgin = "";
+    };
 
-    let setUpListener = (path)=>{
-      console.log('* rtdb-get setUpListener for path '+path)
-      if(path){
-        this.admin.database().ref(path).once('value').then((res)=>{
-          cb(res)
-        }, (fail)=>{
-          console.log('rtdb-get failure ')
-          console.dir(fail)
-        })
-      }  else {
-        console.log('----- rtdb-get got empty path !!')
-        console.dir(config)
+    let setUpListener = (path) => {
+      console.log("* rtdb-get setUpListener for path " + path);
+      if (path) {
+        this.admin
+          .database()
+          .ref(path)
+          .once("value")
+          .then(
+            (res) => {
+              cb(res);
+            },
+            (fail) => {
+              console.log("rtdb-get failure ");
+              console.dir(fail);
+            }
+          );
+      } else {
+        console.log("----- rtdb-get got empty path !!");
+        console.dir(config);
       }
+    };
+
+    if (config.cred) {
+      let c = RED.nodes.getNode(config.cred);
+      this.admin = c.admin;
     }
 
-    if(config.cred){
-      let c = RED.nodes.getNode(config.cred)
-      this.admin = c.admin
+    this.path = config.path;
+    if (this.path) {
+      setUpListener(this.path);
     }
 
-    //console.log('------------------------------- rtdg-get config')
-    //console.dir(config)
-    this.path = config.path
-    if(this.path){
-      setUpListener(this.path)
-    }
-
-
-    //console.log('configuring rtdb-get to listen for messages')
-    node.on('input', function(msg) {
-      let path = this.path
-      if(msg && msg.payload){
-        path = path || msg.payload.path
-        msgin = msg
-        setUpListener(path)
-      }
-    }.bind(this));
-
-
+    node.on(
+      "input",
+      function (msg) {
+        let path = this.path;
+        if (msg && msg.payload) {
+          path = path || msg.payload.path;
+          msgin = msg;
+          setUpListener(path);
+        }
+      }.bind(this)
+    );
   }
   RED.nodes.registerType("rtdb-get", FirebaseAdmin);
-}
+};
